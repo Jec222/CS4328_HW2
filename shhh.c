f1148c9ae693d448a87655adb943ffdf1f702fd5
Jeremias <jec222@txstate.edu>
2021-04-11 17:41:10 -0500

    pipe between child process work, < ,> work

--- shhh.c
+++ shhh.c
@@ -58,7 +58,6 @@ int main(void)
 		/* Your change starts from here */
 		//1.preprocessing the array 
 
- 		int argv_size = sizeof(argv) / sizeof(argv[0]);
 		bool input_redir = false;
 		char*  input_redir_file;
 		char*  output_redir_file;
@@ -66,51 +65,52 @@ int main(void)
 		bool pipe_flag = false;
 		int pipe_counter = 0;
 
-			// the start of each process 
-		int start[20] = {0};
-		printf("%d\n",inword);
-		for (int k = 0; k < 1; k++)
+
+			
+		int start[20] = {0}; // the start of each process 
+ 
+		for (int k = 0; k < m; k++) // m size of argv
 		{
 			if (strcmp (argv[k], "<") == 0)
 			{
 				argv[k] = 0;
-				input_redir_file = argv[k + 1];
+				input_redir_file = argv[k + 1];//strdup(argv[k + 1]);
 				input_redir = true;
 			}
-/*			if (strcmp (argv[k], ">") == 0) 
+			else if (strcmp (argv[k], ">") == 0) 
 			{
 				argv[k] = 0;
 				output_redir_file = argv[k + 1];
-				printf("\n");
-				//printf(output_redir_file);
-				printf("\n");
 				output_redir = true;
 			}
-			if (strcmp (argv[k], "|") == 0)
+			else if (strcmp (argv[k], "|") == 0)
 			{
 				pipe_flag = true;
 				argv[k] = 0;
+				start[pipe_counter + 1] = k + 1; // save position after pipe
 				pipe_counter++;
-				start[pipe_counter] = k + 1; // save position after pipe
-			}*/
+			}
+
 		}
 
 		int l_fd[2]; // file description table
 		int r_fd[2];
-		int pid;
+		//int pid;
 		// need to create the child processes & pipes
 		for (int k = 0; k <= pipe_counter; k++)
 		{
-			if (pipe_flag && k != pipe_counter)//if (/*there are pipes */ && /*not last child*/)
-			{
+			if (pipe_counter > 0 && k != pipe_counter)//if (/*there are pipes */ && /*not last child*/)
+			{	
+				printf("creating a pipe\n");
 				pipe(r_fd);
 			}
-			pid = fork();
+			int pid = fork();
+
 			if (pid < 0 ) // error
 			{
         		printf("error creating process\n");
 			}
-			else if (pid > 0) // parent 
+			if  (pid > 0) // parent 
 			{
 				if (k > 0)
 				{
@@ -119,48 +119,60 @@ int main(void)
 				}
 				l_fd[0] = r_fd[0]; // saving right pipe to left 
 				l_fd[1] = r_fd[1]; // left pipe obtained by assigment
-
-				//close(1); // ?
+				wait(&status);
 			}
+
 			else // child 
 			{
-				if(input_redir_file && k == 0) // deal with input rederection
+				if((input_redir == true) && (k == 0)) // deal with input rederection
 				{	
-					int file_hadler = open(input_redir_file,777);
+					printf("entered >\n");
+					int file_hadler = open(input_redir_file,O_RDONLY , 0400);//,0777);
 					close(0); // set standard input invalid
 					dup(file_hadler);
 					close(file_hadler);// set invalid return to the system
-					input_redir_file = false;
+					//input_redir_file = false;
 				}
-
-				if (/*there are pipes*/true)
+				if (pipe_counter > 0)
 				{
-					//3 types of connections 
-					close(0);
-					dup(r_fd[0]);
-					//close((r_fd[0]); ? no because next process will use 
-					//close(r_fd[1]); ?
+					if (k == 0)
+					{
+						//printf("Handaling first child\n");
+						close(1);
+						dup(r_fd[1]);
+						close(r_fd[1]);
+						close(r_fd[0]);
+					}
+					else if (k < pipe_counter)
+					{
+						//printf("Handaling middle child\n");
+						close(0);
+						dup(l_fd[0]);
+						close(l_fd[1]);
+						close(l_fd[0]);
+						close(1);
+						close(r_fd[0]);
+                        close(r_fd[1]);
+					}
+					else
+					{
+						//printf("handaling last child\n");
+						close(0);
+						dup(l_fd[0]);
+						close(r_fd[0]); 
+						close(r_fd[1]); 
+					}
 				}
-				if (output_redir_file && k == pipe_counter)/*output redir && last process*/
+				if (output_redir && k == pipe_counter)/*output redir && last process*/
 				{   // deal with output redirection
-					close(1); // close standard output
-					int file_handeler = creat(output_redir_file,O_WRONLY); // create file if does not exit
-					dup(file_handeler);
-					close(file_handeler);
+					int n = creat(output_redir_file,O_WRONLY);//|O_CREAT,0777);
+					close(1);
+					dup(n);
+					close(n);
 				}
-				
-
-				//execp(argv[?],&argv[?]) // last part 
-
+				execvp(argv[start[k]], &argv[start[k]]);
 			}
 		} 
-
-
-		if ( fork() == 0 )
-		{
-			execvp( argv[0], argv );
-			printf ( " didn't exec \n ");
-		}
 		wait(&status);
 	}
 }
